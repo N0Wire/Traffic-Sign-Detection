049a060721a94497b5ce67501142afd7a092b794
oliverdrozdowski <oliver.drozdowski@outlook.com>
2018-07-18 22:22:47 +0200

    Added stuff to main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from skimage import io
 from Localization.selective_search import SelectiveSearch
 from Detection.sign_checker import SignChecker
 from Classifier.dataloader import image
+from Classifier.signname import gtsrb_signname
+from Classifier.trainer import import_classifier
+from Classifier.network_utils import predict
 
 ###########################################
 """
@@ -17,12 +20,26 @@ detector over them.
 ###########################################
 
 def find_traffic_signs(img, visualize=False):
-	#load stuff
+	"""
+	This function applies the entire algorithm on an image represented as a numpy array (e.g. from imread)
+	First bounding boxes for region proposals are calculated via the Selective Search Algorithm
+	All these bounding boxes are fed into the SVM to determine, which bounding boxes actually contain
+	a traffic sign.
+	After this the Conv.Neur.Net. with Spat.Transf.Net. is used to classify the following bounding
+    boxes.
+	Lastly, the image is visualized with bounding boxes if wanted
+	Arguments:     img - image as numpy array
+                    visualize - (boolean) whether to plot and save the image with bounding boxes
+	Return:    bounding_list - list of bounding boxes in format []
+                ClassIds - list of corresponding classids
+                TrafficSignNames - ist of corresponding names of signs
+	"""
+    #load stuff
 	s_search = SelectiveSearch()	#selective search
 	checker = SignChecker()			#SVM
 	checker.load("Detection/SVMs/11_")
 	
-    #Network here
+    # Create preprocessor and import pretrained model
 	preprocessor = preprocessor()
 	model,_,_ = import_classifier()
     
@@ -30,20 +47,29 @@ def find_traffic_signs(img, visualize=False):
 	proposals = s_search.run(img, method="deep")
 	signs = checker.check(img, proposals)
 	
+	# Create list of image objects, which are needed for the CNN_STN classifier    
 	Images = [] #list of image objects
 	for s in signs:
 		im = image("", -1, preprocessor, img[s[0]:s[2]+1,s[1]:s[3]+1])
 		Images.append(im)
 	
-	#put images in classifier
+	# Feed all the image objects into the classifier to obtain the classids and names
 	ClassIds = []
-	TrafficSigns = []
-
+	TrafficSignNames = []
+    
 	for image in Images:
-        prediction = predict(model, image)
-        ClassIds.append(prediction)
-        signname = 
-
+		# Predict class id 
+		prediction = predict(model, image)
+		ClassIds.append(prediction)
+        
+		# Look up name corresponding to classid
+		signname = gtsrb_signname(prediction)
+		TrafficSignNames.append(signname)
+        
+	bounding_list = signs
+	        
+	return bounding_list, ClassIds, TrafficSignNames
+    
 if __name__ == "__main__":
 	#load images
 	
